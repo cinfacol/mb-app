05e6c82406eb05fe161e57a3cb8f8940e3ebab5b
Jorge <cinfacol@gmail.com>
2021-03-15 19:41:14 -0500

    New updates for Heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2dggh%6&n)oob*432*7@rvxll&%0*lq+9p&j7#tgxl7%86w663'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
